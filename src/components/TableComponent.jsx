cfceab66f1acf1e57c4d2a39bceef18dbc3c6754
PhyPhy07 <168453204+PhyPhy07@users.noreply.github.com>
2024-08-25 13:34:34 -0500

    got all the gallery to the top

---
+++ src/components/TableComponent.jsx
@@ -0,0 +1,64 @@
+
+
+// TableComponent.jsx
+import React from 'react';
+import { useTable } from 'react-table';
+
+import TeamRoster from './Team_Roster'; // if both files are in the same directory
+
+
+
+const TableComponent = () => {
+  const data = React.useMemo(() => TeamRoster, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Score",
+        accessor: "score",
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  return (
+    <div className="table-container">
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableComponent;
