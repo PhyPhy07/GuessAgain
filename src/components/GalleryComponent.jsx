a571a38ea4fe8e9fd4c25105ac136d298d95c13a
PhyPhy07 <168453204+PhyPhy07@users.noreply.github.com>
2024-08-26 09:04:37 -0500

    worked on gallery

--- src/components/GalleryComponent.jsx
+++ src/components/GalleryComponent.jsx
@@ -11,10 +11,11 @@ import hotelpoolsImg from '../images/hotelpools.jpg';
 import iJustDiedImg from '../images/I-Just-Died-In-Your-Arms.jpg';
 import rhythmDancerImg from '../images/rhythm is a dancer.jpeg';
 import sawyerImg from '../images/sawyer.jpeg';
-import purityImg from '../images/purity.jpg';
+import purityImg from '../images/purity.jpeg';
 import theflaming from '../images/theflaming.jpg';
 import kiss from '../images/kiss.jpg';
 import pinkpony from '../images/pinkpony.jpg';
+import dj from '../images/dj.png';
 
 const GalleryComponent = () => {
   const galleryContainerRef = useRef(null);
@@ -67,6 +68,7 @@ const GalleryComponent = () => {
   return (
     <div className="gallery-container">
       <div ref={galleryContainerRef} className="gallery">
+      <div className="gallery-item"><img src={dj} alt="DJ" /></div>
         <div className="gallery-item"><img src={brokenImg} alt="Broken" /></div>
         <div className="gallery-item"><img src={chetImg} alt="Chet" /></div>
         <div className="gallery-item"><img src={evansBlueImg} alt="Evans Blue" /></div>
@@ -80,6 +82,7 @@ const GalleryComponent = () => {
         <div className="gallery-item"><img src={theflaming} alt="The Flaming" /></div>
         <div className="gallery-item"><img src={kiss} alt="Kiss" /></div>
         <div className="gallery-item"><img src={pinkpony} alt="Pink Pony" /></div>
+     <div className="gallery-item"><img src={dj} alt="DJ" /></div>
       </div>
       <div ref={indicatorRef} className="indicator"></div>
     </div>
