b4b292902a0754931393c1b1d302297dcdeba17d
PhyPhy07 <168453204+PhyPhy07@users.noreply.github.com>
2024-09-06 13:28:33 -0500

    keys

--- src/createClient.js
+++ src/createClient.js
@@ -4,3 +4,5 @@ export const supabase = createClient(
   import.meta.env.VITE_APP_SUPABASE_URL,
   import.meta.env.VITE_APP_SUPABASE_ANON_KEY
 );
+console.log('Supabase URL:', import.meta.env.VITE_APP_SUPABASE_URL);
+console.log('Supabase anon key:', import.meta.env.VITE_APP_SUPABASE_ANON_KEY);
