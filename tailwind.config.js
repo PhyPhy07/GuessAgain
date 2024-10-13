1860e93a13969a343cc2bde41a332a9b2b93d4ab
PhyPhy07 <168453204+PhyPhy07@users.noreply.github.com>
2024-10-12 21:04:57 -0500

    got container back to right color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
       colors: {
         'custom-black': '#0f0f0f',
-      }
+      },
+  
     }
   },
   variants: {},
